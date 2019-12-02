011c103346bbd55bdab857f030503f1670b238bd
Jonathan-Eboh <eboh1923@gmail.com>
2019-12-02 14:51:37 -0600

    most recent for website

--- portfolio_site/js/script.js
+++ portfolio_site/js/script.js
@@ -1,9 +1,44 @@
 $(document).ready(function() {
 
-  $('#slides').superslides({
-    animation: 'fade',
-    play: 5000, //milliseconds 5 seconds
-    pagination: false
-  });
+      $('#slides').superslides({
+        animation: 'fade',
+        play: 5000, //milliseconds 5 seconds
+        pagination: false
+      });
 
+      let typed = new Typed(".typed", {
+        strings: ["Software Engineer.", "Web Developer.", "Student."],
+        typeSpeed: 100,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+      });
+
+    
+    
+    
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })    
+    
+    
+    $('.chart').easyPieChart({
+            //your options goes here
+        });
+    
 });
+
+
